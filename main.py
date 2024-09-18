a5dd08d286107254ddf2c77ed07e3e2b181e0d68
HarrisonMustaphaFal <HM314883@falmouth.ac.uk>
2024-09-18 12:44:51 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+#Variables
+
+#]]
+ev3 = EV3Brick()
+leftWheelMotor = Motor(Port.D)
+rightWheelMotor = Motor(Port.C)
+leftButton = TouchSensor(Port.S1)
+rightButton = TouchSensor(Port.S2)
+reverseButton = TouchSensor(Port.S3)
+buttonToggle = false
+
+currentSpeed = 500
+#]]
+
+#Code
+
+def MoveMotor(motor, speed, runTime):
+    if runTime == 0:
+        motor.run(speed)
+    else:
+        motor.run_time(speed, runTime)
+    
+
+
+
+
+##Initialize
+
+leftWheelMotor.brake()
+rightWheelMotor.brake()
+ev3.speaker.beep()
+
+while True:
+    if not leftButton.pressed() and not rightButton.pressed():
+        leftWheelMotor.brake()
+        rightWheelMotor.brake()
+    else:
+        if leftButton.pressed():
+            print("left button pressed")
+            MoveMotor(leftWheelMotor, currentSpeed, 0)
+        if rightButton.pressed():
+            print("right button pressed")
+            MoveMotor(rightWheelMotor, currentSpeed, 0)
+    if reverseButton.pressed():
+        if buttontoggle:
+        currentSpeed = 500
+        buttontoggle = false
+        else:
+        currentSpeed = -500
+        buttontoggle = true
+
